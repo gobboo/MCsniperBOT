223e85fab73142f7eb2d5fe13f34578acd4ace5c
Jordan <jordansdns@gmail.com>
2021-05-18 00:19:38 +0100

    Added base for message deleted logs

---
+++ cogs/events/messages.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+
+class Messages(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_raw_bulk_message_delete(self, payload):
+        print(len(payload.message_ids))
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        print(message.content)
+
+
+def setup(client):
+    client.add_cog(Messages(client))
