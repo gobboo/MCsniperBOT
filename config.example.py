1a57abae50ca5862474215e4ce92a4545c62fe69
Jordan <jordansdns@gmail.com>
2021-05-18 15:52:25 +0100

    Added paste.gg API key

--- config.example.py
+++ config.example.py
@@ -1,2 +1,4 @@
 TOKEN = ""
 PREFIX = ""
+LOGO = "https://cdn.discordapp.com/attachments/819654216548483083/844153348470734858/logo.png"
+PASTE_API = ""  # Uses paste.gg API, register and create API Key
