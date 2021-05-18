dd38c7f4d6b59362a51c994f922de88126f47480
Jordan <jordansdns@gmail.com>
2021-05-18 17:54:40 +0100

    Replaced raw_bulk_message_delete with regular bulk_message_delete to support paste logging

--- cogs/events/messages.py
+++ cogs/events/messages.py
@@ -1,7 +1,10 @@
-# import discord
+from datetime import datetime
+
 from discord.ext import commands
 
+from utils.functions import create_paste_desc
 from utils.logs import log
+from utils.logs import paste
 
 
 class Messages(commands.Cog):
@@ -9,22 +12,41 @@ class Messages(commands.Cog):
         self.client = client
 
     @commands.Cog.listener()
-    async def on_raw_bulk_message_delete(self, payload):
-        amount = len(payload.message_ids)
-        cached_messages = payload.cached_messages
+    async def on_bulk_message_delete(self, messages):
+        amount = len(messages)
+        channels = []
         user_count = {}
         list_count = ""
-        for message in cached_messages:
+        for message in messages:
+            if message.channel not in channels:
+                channels.append(message.channel)
             if message.author.id in user_count:
                 user_count[message.author.id] = user_count[message.author.id] + 1
             else:
                 user_count[message.author.id] = 1
         for user_id, message_count in user_count.items():
             list_count += f"<@!{user_id}> - {message_count}\n"
+
+        channel = (
+            f"{len(channels)} channels" if len(channels) > 1 else f"<#{channels[0].id}>"
+        )
+        description = (
+            f"`{amount}` cached messages bulk deleted from {channel}\n\n{list_count}"
+        )
+
+        text_to_paste = await create_paste_desc(messages)
+        paste_url = await paste(
+            name=f"{datetime.now().strftime('%d|%m - %H:%M')} Purged Messages",
+            description=f"{len(messages)} messages deleted from {channel}",
+            to_paste=text_to_paste,
+        )
+        if paste_url is not None:
+            description += f"\n[Purge Log]({paste_url})"
+
         await log(
             client=self.client,
             title="Messages Deleted!",
-            description=f"`{amount}` messages bulk deleted from <#{payload.channel_id}>\n\n{list_count}",
+            description=description,
         )
 
     @commands.Cog.listener()
