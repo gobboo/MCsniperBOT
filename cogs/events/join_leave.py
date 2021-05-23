bb29ea81ed65b3a633f8688d0cddbd2bea46ccac
Jordan <jordansdns@gmail.com>
2021-05-24 00:59:20 +0100

    Fixed the logic

--- cogs/events/join_leave.py
+++ cogs/events/join_leave.py
@@ -1,14 +1,14 @@
+import datetime
+import io
+from datetime import datetime as dt
+
+import discord
 from discord.ext import commands
 from discord.utils import get
 
-import discord
-
-from datetime import datetime as dt
-import datetime
-import io
-
+from database.postgres_handler import execute_sql
+from database.postgres_handler import query_sql
 from utils import captcha
-from database.postgres_handler import execute_sql, query_sql
 
 
 class Welcome(commands.Cog):
@@ -17,7 +17,7 @@ class Welcome(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        if dt.utcnow() - member.created_at < datetime.timedelta(days=3):
+        if dt.utcnow() - member.created_at > datetime.timedelta(days=3):
             await member.add_roles(get(member.guild.roles, name="member"))
         else:
             # The account was brand new (under 3 days old)
@@ -29,14 +29,16 @@ class Welcome(commands.Cog):
                     description=f"Hello {member.mention}, your Discord account is less than 3 days old."
                     " Due to this, we require you to solve the below captcha in order to gain access to the rest of the server.\n\n"
                     "Please respond with the exact text shown in the captcha below.",
-                    color=int("d8737f", 16)
+                    color=int("d8737f", 16),
                 )
 
                 embed.set_image(url="attachment://captcha.png")
                 await member.dm_channel.send(
                     content=member.mention,
                     embed=embed,
-                    file=discord.File(io.BytesIO(captcha_bytes.getvalue()), filename="captcha.png")
+                    file=discord.File(
+                        io.BytesIO(captcha_bytes.getvalue()), filename="captcha.png"
+                    ),
                 )
                 execute_sql(
                     f"""INSERT INTO captcha_users (
