badf3f8ccca4c06c84376c8f18aa223a60c7f586
Jordan <jordansdns@gmail.com>
2021-05-24 01:16:40 +0100

    Ran a little clean up, moved query/executes to appropriate files

--- cogs/events/join_leave.py
+++ cogs/events/join_leave.py
@@ -3,12 +3,13 @@ import io
 from datetime import datetime as dt
 
 import discord
+from discord import Forbidden
 from discord.ext import commands
 from discord.utils import get
 
-from database.postgres_handler import execute_sql
 from database.postgres_handler import query_sql
-from utils import captcha
+from database.users import set_captcha
+from utils.functions import gen_captcha
 
 
 class Welcome(commands.Cog):
@@ -18,50 +19,35 @@ class Welcome(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         if dt.utcnow() - member.created_at > datetime.timedelta(days=3):
-            await member.add_roles(get(member.guild.roles, name="member"))
+            await member.add_roles(get(member.guild.roles, name="Member"))
         else:
             # The account was brand new (under 3 days old)
-            if member.dm_channel is None:
-                await member.create_dm()
             try:
-                text, captcha_bytes = captcha.gen_captcha()
+                captcha, captcha_bytes = gen_captcha()
                 embed = discord.Embed(
-                    description=f"Hello {member.mention}, your Discord account is less than 3 days old."
-                    " Due to this, we require you to solve the below captcha in order to gain access to the rest of the server.\n\n"
-                    "Please respond with the exact text shown in the captcha below.",
+                    description=f"Hello {member.mention}, your Discord account is less than 3 days old. "
+                    f"Due to this, we require you to solve the below captcha in order to gain access to "
+                    f"the rest of the server.\n\n"
+                    f"Please respond with the exact text shown in the captcha below.",
                     color=int("d8737f", 16),
                 )
 
                 embed.set_image(url="attachment://captcha.png")
-                await member.dm_channel.send(
+                await member.send(
                     content=member.mention,
                     embed=embed,
                     file=discord.File(
                         io.BytesIO(captcha_bytes.getvalue()), filename="captcha.png"
                     ),
                 )
-                execute_sql(
-                    f"""INSERT INTO captcha_users (
-                        user_id,
-                        captcha,
-                        attempts
-                    ) VALUES ('{member.id}', '{text}', 1);"""
+                await set_captcha(member.id, captcha)
+            except Forbidden:
+                print(
+                    "Here we will tag them in the one channel they can see, telling them to enable server DMs then "
+                    "running !verify"
                 )
-                print(query_sql("SELECT * FROM captcha_users"))
-
-                # def check(m):
-                #     return m.channel == member.dm_channel and m.author == member
-                # response = await self.client.wait_for("message", check=check)
-                # print(response.content, text)
-                # if response.content.lower().strip() == text:
-                #     await member.dm_channel.send("successfully answered captcha!")
-                #     await member.add_roles(get(member.guild.roles, name="member"))
-                # else:
-                #     print("failed captcha rippp")
-                #     await member.dm_channel.send("failed captcha")
             except Exception as e:
                 print(e)
-                # handle them not having dms enabled
 
 
 def setup(client):
