bc9d0fedcfde964e76ef605faaa1f96b5fcc5914
Jordan <jordansdns@gmail.com>
2021-05-25 00:50:03 +0100

    Added success responses

--- utils/responses.py
+++ utils/responses.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import discord
 
 from config import LOGO
@@ -5,10 +7,25 @@ from config import LOGO
 
 async def generate_error(ctx, error, example=None):
     error_embed = discord.Embed(
-        title="Error!", colour=int("fb607f", 16), description=error
+        title="Error!",
+        colour=int("fb607f", 16),
+        description=error,
+        timestamp=datetime.utcnow(),
     )
     if example is not None:
         error_embed.set_footer(text=example, icon_url=LOGO)
     else:
         error_embed.set_footer(text="​", icon_url=LOGO)
+
     return await ctx.send(embed=error_embed)
+
+
+async def generate_success(ctx, success_message):
+    success_embed = discord.Embed(
+        title="Success!",
+        colour=0x000000,
+        description=success_message,
+        timestamp=datetime.utcnow(),
+    )
+    success_embed.set_footer(text="​", icon_url=LOGO)
+    return await ctx.send(embed=success_embed)
