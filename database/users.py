16f44cfcd0d3f1bd4651ede646c1c2a864eae0f5
Jordan <jordansdns@gmail.com>
2021-05-24 01:34:20 +0100

    Added all captcha queries to users db queries

--- database/users.py
+++ database/users.py
@@ -1,6 +1,10 @@
 from database.postgres_handler import execute_sql
 from database.postgres_handler import query_sql
 
+"""
+Levelling System Queries
+"""
+
 
 async def user_exists(user_id: int) -> bool:
     if query_sql(f"SELECT * FROM users WHERE user_id={user_id}") is None:
@@ -15,7 +19,7 @@ async def create_user(user_id: int):
 async def get_xp(user_id: int) -> int:
     if not await user_exists(user_id):
         await create_user(user_id)
-    return query_sql(f"SELECT experience FROM users WHERE user_id={user_id}", True)[0]
+    return query_sql(f"SELECT experience FROM users WHERE user_id={user_id}")[0]
 
 
 async def increment_xp(user_id: int, xp_gained: int):
@@ -26,3 +30,32 @@ async def increment_xp(user_id: int, xp_gained: int):
 
 async def increment_messages(user_id: int):
     execute_sql(f"UPDATE users SET messages=messages + 1 WHERE user_id={user_id}")
+
+
+"""
+Captcha Queries
+"""
+
+
+async def get_captcha_data(user_id: int) -> list:
+    return query_sql(
+        f"SELECT user_id, captcha, attempts FROM captcha_users WHERE user_id={user_id}"
+    )
+
+
+async def require_captcha(user_id: int) -> bool:
+    if query_sql(f"SELECT * FROM captcha_users WHERE user_id={user_id}") is None:
+        return False
+    return True
+
+
+async def set_captcha(user_id: int, captcha: str):
+    execute_sql(
+        f"INSERT INTO captcha_users (user_id, captcha, attempts) VALUES ({user_id}, '{captcha}', 1)"
+    )
+
+
+async def increment_attempts(user_id):
+    execute_sql(
+        f"UPDATE captcha_users SET attempts=attempts + 1 WHERE user_id={user_id}"
+    )
