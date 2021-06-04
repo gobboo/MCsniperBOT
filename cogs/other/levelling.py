0c5e0df7bad340475163f4383797cbe1ae9bed2c
Jordan <jordansdns@gmail.com>
2021-06-04 20:47:34 +0100

    Allowed the ability to check other users rank,

--- cogs/other/levelling.py
+++ cogs/other/levelling.py
@@ -11,8 +11,10 @@ class Levelling(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def rank(self, ctx):
-        card = await generate_rank_card(ctx.message.author)
+    async def rank(self, ctx, user: discord.Member = None):
+        # Need to add a rank_error func in case invalid user
+        user = ctx.message.author if user is None else user
+        card = await generate_rank_card(user)
         byte_array = io.BytesIO()
         card.save(byte_array, format="png")
         file = discord.File(io.BytesIO(byte_array.getvalue()), filename="rank.png")
